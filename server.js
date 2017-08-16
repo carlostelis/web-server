33e7beb928fc90f0a300ea967d4e846e3623d8f8
Carlos Telis <carlos.telis@outlook.com>
2017-08-16 00:09:56 -0500

    Inicializa repositorio

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+var app = express();
+const puerto = 3000;
+
+// middleware define una función para ser ejecutada al llamar cualquier ruta del sitio desde el navegador, o en las que sea directamente especificado
+var middleware = {
+    requireAuthentication: function(req, res, next) {
+        console.log('private route hit');
+        next();
+    },
+    logger: function(req, res, next) {
+        console.log(`${new Date().toString()}: ${req.method} on ${req.url}`);
+        next();
+    }
+};
+
+
+// Es importante poner el middleware antes de la app, de lo contrario no se ejecutará
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+// app.get('/', function(req, res) {
+//     res.send(`Hello express`);
+// });
+
+// Al ponerlo en medio de un get(), se especifica solo para esa ruta
+app.get('/about', middleware.requireAuthentication, function(req, res) {
+    res.send(`About us`);
+});
+
+// Para despligue de la página por defecto
+app.use(express.static(__dirname + '/public'));
+
+app.listen(puerto, function() {
+    console.log(`Servidor escuchando el puerto ${puerto}`);
+});
